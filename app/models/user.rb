8fb8ba9f611344cd55abdbc9c485ba03843174e1
RaulEV1411 <136620455+RaulEV1411@users.noreply.github.com>
2023-11-08 13:03:43 -0600

    prueba

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_many_attached :photo
-  rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
-end
